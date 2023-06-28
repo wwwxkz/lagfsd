803fe286cd9e8816f3b39497bb207defc3f71e41
wwwxkz <wxkzwin@gmail.com>
2023-06-29 00:36:47 +0530

    Fixed Dockerfile for Springboot and changed from Gradle to Maven

--- springboot/src/main/java/com/springboot/gateway/GatewayApplication.java
+++ springboot/src/main/java/com/springboot/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.springboot.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class GatewayApplication {
 
 	public static void main(String[] args) {
